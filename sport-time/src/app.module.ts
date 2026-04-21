7cafcd649a11497b361b9a24ad0a602b740f54fa
HectorDaniel-00 <danielvargas.nike@gmail.com>
2026-04-21 11:53:26 -0500

    Gestión.
    - Se crearon `DatabaseModule` y `DatabaseService` para la gestión de conexiones a MongoDB.
    
    - Se agregó el esquema de validación Joi para variables de entorno.
    
    - Se actualizaron las dependencias en `package.json` y `package-lock.json` para los nuevos módulos.

--- sport-time/src/app.module.ts
+++ sport-time/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/validation-schema';
+import { DatabaseModule } from './config/database/database.module';
 
 @Module({
-  imports: [],
+  imports: 
+  [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: envValidationSchema,
+      isGlobal: true
+    }), 
+    DatabaseModule
+  ],
   controllers: [],
   providers: [],
 })
